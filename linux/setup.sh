180a9af9729c4eef38d7b8a81dcb0985c13810c7
Lukas Alexandre <lukas@codelogic.me>
2018-09-06 09:59:04 +0200

    Some extra linux specific config to improve battery and Gvim experience

--- linux/setup.sh
+++ linux/setup.sh
@@ -2,3 +2,6 @@ rm ~/.vimrc ~/.zshrc
 ln -s ../rcs/vimrc ~/vimrc
 ln -s ../rcs/zshrc ~/zshrc
 ln -s ../linux/keys.map ~/.Xmodmap
+
+# Set refresh rate down to 60hz to save battery
+xrandr -s 1920x1080 -r 60~
