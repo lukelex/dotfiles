9197d84f6f78c346fb7837b70f06d46b59f39d48
Lukas Alexandre <lukas@codelogic.me>
2018-09-22 21:36:42 +0200

    Adding a basic i3 config

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,7 +1,10 @@
-rm ~/.vimrc ~/.zshrc
-ln -s ~/dotfiles/rcs/vimrc ~/.vimrc
-ln -s ~/dotfiles/rcs/zshrc ~/.zshrc
-ln -s ~/dotfiles/linux/keys.map ~/.Xmodmap
+rm ~/.vimrc ~/.zshrc ~/.Xmodmap ~/.i3/config
+ln -fvs ~/dotfiles/rcs/vimrc ~/.vimrc
+ln -fvs ~/dotfiles/rcs/zshrc ~/.zshrc
+ln -fvs ~/dotfiles/linux/keys.map ~/.Xmodmap
+
+mkdir ~/.i3
+ln -fvs ~/dotfiles/linux/i3/config ~/.i3/config
 
 # Set refresh rate down to 60hz to save battery
 xrandr -s 1920x1080 -r 60~
