cb7d9f545b991dc912675064941be3d9f66961af
Lukas Alexandre <lukas@codelogic.me>
2018-10-13 12:02:41 +0200

    Doing installations first and symlinking after.
    This is due to some post install scripts touching files that were
    previously symlinked

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,17 +1,4 @@
 rm ~/.profile ~/.vimrc ~/.zshrc ~/.Xmodmap ~/.i3/config
-ln -fvs ~/dotfiles/rcs/vimrc ~/.vimrc
-ln -fvs ~/dotfiles/rcs/zshrc ~/.zshrc
-ln -fvs ~/dotfiles/linux/keys.map ~/.Xmodmap
-ln -fvs ~/dotfiles/linux/profile ~/.profile
-
-if [[ $(sudo dmidecode --string chassis-type) -eq "Desktop" ]]; then
-  ln -fvs ~/dotfiles/linux/i3/deskjaro-blocks ~/.i3blocks.conf
-else
-  ln -fvs ~/dotfiles/linux/i3/blade-blocks ~/.i3blocks.conf
-fi
-
-mkdir ~/.i3
-ln -fvs ~/dotfiles/linux/i3/config ~/.i3/config
 
 pamac install \
   i3blocks rofi sysstat xdotool xclip gconf \
@@ -25,3 +12,17 @@ yaourt -S --noconfirm whatsapp-web-desktop vundle postgresql-9.6 dropbox woeusb
 git clone git@github.com:vivien/i3blocks-contrib.git ~/i3blocks-contrib
 
 curl -L http://install.ohmyz.sh | sh
+
+ln -fvs ~/dotfiles/rcs/vimrc ~/.vimrc
+ln -fvs ~/dotfiles/rcs/zshrc ~/.zshrc
+ln -fvs ~/dotfiles/linux/keys.map ~/.Xmodmap
+ln -fvs ~/dotfiles/linux/profile ~/.profile
+
+if [[ $(sudo dmidecode --string chassis-type) -eq "Desktop" ]]; then
+  ln -fvs ~/dotfiles/linux/i3/deskjaro-blocks ~/.i3blocks.conf
+else
+  ln -fvs ~/dotfiles/linux/i3/blade-blocks ~/.i3blocks.conf
+fi
+
+mkdir ~/.i3
+ln -fvs ~/dotfiles/linux/i3/config ~/.i3/config
