9006ad74a98ccda9dd5fc454469fdb10c4665401
Lukas Alexandre <lukas@codelogic.me>
2018-11-16 10:34:25 +0100

    Fixing package installation for AUR packages

--- linux/setup.sh
+++ linux/setup.sh
@@ -6,15 +6,22 @@ rm -rf ~/.profile ~/.vimrc ~/.zshrc ~/.Xmodmap ~/.i3/config
 pamac install \
   i3blocks rofi sysstat xdotool xclip gconf cmake autorandr \
   redis docker docker-compose ttf-hack jq \
-  gvim ctags zsh diff-so-fancy the_silver_searcher zeal \
+  gvim ctags zsh zsh-completions diff-so-fancy \
+  rubygems the_silver_searcher zeal \
   firefox etcher viewnior telegram-desktop playerctl \
   flameshot variety vlc redshift timeshift
 
-yaourt_libs=( google-chrome-stable slack-desktop whatsapp-web-desktop otf-font-awesome-5-free postgresql-9.6 dropbox woeusb spotify debtap )
-for i in "${yaourt_libs[@]}"
-do
-  ~/dotfiles/linux/scripts/yaourt $i
-done
+yaourt -S --noconfirm google-chrome-stable \
+  slack-desktop \
+  whatsapp-web-desktop \
+  otf-font-awesome-5-free \
+  postgresql-9.6 \
+  nvm \
+  phantomjs \
+  dropbox \
+  woeusb \
+  spotify \
+  debtap
 
 git clone git@github.com:vivien/i3blocks-contrib.git ~/i3blocks-contrib
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
