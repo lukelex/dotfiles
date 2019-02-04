53117022812252d6707b41356d9a1ca2e928e335
Lukas Alexandre <lukas@codelogic.me>
2019-02-04 11:48:32 +0100

    Adding QMK keymap configs for my mechanical keyboards

--- keyboards/kbd6x/keymap.c
+++ keyboards/kbd6x/keymap.c
@@ -6,15 +6,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
     MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, MO(1),
-    _______,              KC_LALT, KC_LGUI,                   LT(1, KC_SPACE),            DF(2),   KC_RALT,  _______
+    XXXXXXX,              KC_LALT, KC_LGUI,                   LT(1, KC_SPACE),            DF(2),   KC_RALT,  XXXXXXX
     ),
 
 [1] = LAYOUT(
-    _______,              KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_MNXT, KC_MPLY,
-    _______,              _______, _______, _______, _______, _______,  _______, _______, _______, _______,  KC_PSCR, _______, _______, _______,
-    _______,              KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, _______,
-    _______,              _______, _______, _______, _______, _______,  _______, _______, _______, _______,  _______, _______, _______,
-    _______,              _______, _______,                   _______,                    _______, MO(3),    _______
+    XXXXXXX,              KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_MNXT, KC_MPLY,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,              KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX,                   XXXXXXX,                    XXXXXXX, MO(3),    XXXXXXX
     ),
 
 [2] = LAYOUT(
