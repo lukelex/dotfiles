99051c1adc4f1d2682f011cfa13c375b03487ffa
Lukas Alexandre <lukas@codelogic.me>
2020-03-22 10:18:26 +0100

    [Linux] Writing a simpler USB mount/unmount script using Rofi

---
+++ linux/scripts/usb-mounter.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+set -e
+
+drives=$(lsblk  --noheadings --raw | awk '$1~/s.*[[:digit:]]/' | awk '{print "/dev/"$1,$7}')
+mounted=$(echo $drives | awk '$3!=""' | awk '{print "unmount",$1}')
+unmounted=$(echo $drives | awk '$3==""' | awk '{print "mount",$1}')
+
+selected=$(echo "$mounted $unmounted" | rofi show -dmenu -p "drives")
+[[ -z $selected ]] && exit
+
+action=$(echo $selected | awk '{print $1}')
+drive=$(echo $selected | awk '{print $2}')
+
+result=$(udisksctl $action --block-device $drive)
+
+if [[ "$action" = "mount" ]]; then
+  notify-send 'USB drive' 'Successfully mounted' --icon=dialog-information
+else
+  notify-send 'USB drive' 'Successfully unmounted' --icon=dialog-information
+fi
