bcb3f814c3f65314305148ce2095a8a088befdef
Lukas Alexandre <lukas@codelogic.me>
2020-04-12 20:42:59 +0200

    [Keyboards] Updating H87a layout configuration & adding Via config

--- keyboards/h87a/keymap.c
+++ keyboards/h87a/keymap.c
@@ -16,7 +16,6 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
   [0] = LAYOUT_all(
     KC_ESC,                        KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,                  KC_MPRV, KC_MPLY, KC_MNXT,
     KC_GRV,               KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS,   KC_ESC,       KC_INS,  KC_HOME, KC_PGUP,
@@ -38,48 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,               XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX,
     KC_TAB,               XXXXXXX, KC_UP,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX,
     MOD_LCTL,             KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_LSFT, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT,  KC_MPLY, XXXXXXX,          XXXXXXX,                          KC_UP,
-    KC_LCTL,              KC_LGUI, KC_LALT,                    KC_SPC,                                       XXXXXXX, KC_RALT, XXXXXXX, MO(3),                   KC_LEFT, KC_DOWN, KC_RGHT),
+    KC_LSFT, XXXXXXX,     KC_MPRV, KC_MNXT, KC_MPLY,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT,  KC_MPLY, XXXXXXX,          XXXXXXX,                          KC_UP,
+    KC_LCTL,              XXXXXXX, KC_LGUI,                    KC_TRNS,                                      XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,                 KC_LEFT, KC_DOWN, KC_RGHT),
 
   [3] = LAYOUT_all(
-    RESET,                         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,  RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI,                 BL_DEC,  BL_TOGG, BL_INC,
+    RESET,                         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   TG(1),        XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    RGB_TOG,              RGB_HUI, RGB_SAI, RGB_VAI,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX,
+    RGB_MOD,              RGB_HUD, RGB_SAD, RGB_VAD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,          XXXXXXX,                          XXXXXXX,
-    XXXXXXX,              XXXXXXX, XXXXXXX,                    XXXXXXX,                                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX),
-
+    XXXXXXX,              XXXXXXX, XXXXXXX,                    XXXXXXX,                                      XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,                 XXXXXXX, XXXXXXX, XXXXXXX),
 };
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-void led_init_ports(void) {
-  DDRD |= (1<<5); // OUT
-  DDRE |= (1<<6); // OUT
-}
-
-void led_set_user(uint8_t usb_led) {
-
-  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-    DDRD |= (1 << 5); PORTD &= ~(1 << 5);
-  } else {
-    DDRD &= ~(1 << 5); PORTD &= ~(1 << 5);
-  }
-
-  if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-    DDRE |= (1 << 6); PORTE &= ~(1 << 6);
-  } else {
-    DDRE &= ~(1 << 6); PORTE &= ~(1 << 6);
-  }
-
-}
