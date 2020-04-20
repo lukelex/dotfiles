73ada59d2bde7abce26eeae2c41b58c51031877c
Lukas Alexandre <the.lukelex@gmail.com>
2020-04-20 17:45:19 +0200

    [i3] Adding a better looking custom lockscreen

--- linux/setup.sh
+++ linux/setup.sh
@@ -13,7 +13,7 @@ pamac install postgresql-10 redis docker docker-compose
 pamac install slack-desktop whatsapp-nativefier telegram-desktop
 pamac install dropbox woeusb spotify via-bin stress
 pamac install avr-gcc dfu-programmer avrdude gcc-arm-none-eabi-bin
-pamac install imagemagick-full neofetch
+pamac install imagemagick-full neofetch betterlockscreen
 pamac install ttf-hack nerd-fonts-complete noto-fonts-cjk
 
 mkdir -p ~/.i3
@@ -30,3 +30,6 @@ ln -fvs ~/dotfiles/linux/profile ~/.profile
 ln -fvs ~/dotfiles/linux/xprofile ~/.xprofile
 ln -fvs ~/dotfiles/linux/keys.map ~/.Xmodmap
 ln -fvs ~/dotfiles/linux/peripherals/$(hostname).conf /etc/X11/xorg.conf
+sudo ln -fvs ~/dotfiles/scripts/exit /usr/bin/i3exit
+
+betterscreenlock -u ~/dotfiles/wallpapers/zelda-horse-walk.jpg
