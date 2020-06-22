aeb3f217ea26592c329bd14ccfc91425f5c03feb
Lukas Alexandre <the.lukelex@gmail.com>
2020-06-22 10:16:00 +0200

    [Keyboards] Adding basic accent support to the Leaf60

--- keyboards/leaf60/keymap.c
+++ keyboards/leaf60/keymap.c
@@ -17,30 +17,41 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_all(
-    KC_GRV,               KC_1,    KC_2,    KC_3,     KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
-    KC_TAB,               KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-    MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,     KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT,              XXXXXXX, KC_Z,    KC_X,     KC_C,    KC_V,          KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
-    XXXXXXX,              KC_LALT, KC_LGUI, KC_SPC,   MO(2),   LT(2, KC_ENT), KC_RGUI, KC_RALT, MO(3),   XXXXXXX),
+    KC_GRV,               KC_1,    KC_2,    KC_3,       KC_4,    KC_5,          KC_6,       KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS,       KC_EQL,  KC_BSLS, KC_ESC,
+    KC_TAB,               KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,          KC_Y,       KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC,       KC_RBRC, KC_BSPC,
+    MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,       KC_F,    KC_G,          KC_H,       KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,       KC_ENT,
+    KC_LSFT,              XXXXXXX, KC_Z,    KC_X,       KC_C,    KC_V,          KC_B,       KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,       KC_RSFT, XXXXXXX,
+    XXXXXXX,              KC_LALT, KC_LGUI, KC_SPC,     MO(2),   LT(2, KC_ENT), MO(3),      KC_RALT, MO(4),   XXXXXXX),
 
   LAYOUT_all(
-    KC_GRV,               KC_1,    KC_2,    KC_3,     KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
-    KC_TAB,               KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-    KC_LCTL,              KC_A,    KC_S,    KC_D,     KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT,              XXXXXXX, KC_Z,    KC_X,     KC_C,    KC_V,          KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
-    XXXXXXX,              KC_LALT, KC_LGUI, KC_SPC,   MO(2),   KC_ENTER,      KC_RGUI, KC_RALT, MO(3),   XXXXXXX),
+    KC_GRV,               KC_1,    KC_2,    KC_3,       KC_4,    KC_5,          KC_6,       KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS,       KC_EQL,  KC_BSLS, KC_ESC,
+    KC_TAB,               KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,          KC_Y,       KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC,       KC_RBRC, KC_BSPC,
+    KC_LCTL,              KC_A,    KC_S,    KC_D,       KC_F,    KC_G,          KC_H,       KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,       KC_ENT,
+    KC_LSFT,              XXXXXXX, KC_Z,    KC_X,       KC_C,    KC_V,          KC_B,       KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,       KC_RSFT, XXXXXXX,
+    XXXXXXX,              KC_LALT, KC_LGUI, KC_SPC,     MO(2),   LT(2, KC_ENT), MO(3),      KC_RALT, MO(4),   XXXXXXX),
 
   LAYOUT_all(
-    KC_GRV,               KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX,
-    KC_TAB,               XXXXXXX, KC_UP,   XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_LCTL,              KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX,       KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_LSFT,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, KC_VOLD, KC_VOLU, KC_MPRV,  KC_MNXT, KC_MPLY, XXXXXXX, XXXXXXX,
-    XXXXXXX,              KC_LALT, KC_LGUI, KC_SPC,   XXXXXXX, KC_ENTER,      KC_RGUI, KC_RALT, XXXXXXX, XXXXXXX),
+    KC_GRV,               KC_F1,   KC_F2,   KC_F3,      KC_F4,   KC_F5,         KC_F6,      KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,        KC_F12,  XXXXXXX, XXXXXXX,
+    KC_TAB,               XXXXXXX, KC_UP,   XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  KC_PSCR, XXXXXXX,       XXXXXXX, XXXXXXX,
+    KC_LCTL,              KC_LEFT, KC_DOWN, KC_RIGHT,   XXXXXXX, XXXXXXX,       KC_LEFT,    KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX,       XXXXXXX,
+    KC_LSFT,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    KC_VOLD, KC_VOLU, KC_MPRV,  KC_MNXT, KC_MPLY,       XXXXXXX, XXXXXXX,
+    XXXXXXX,              KC_LALT, KC_LGUI, KC_SPC,     XXXXXXX, KC_ENTER,      KC_RGUI,    KC_RALT, XXXXXXX, XXXXXXX),
 
   LAYOUT_all(
-    RESET,                XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(1),
-    XXXXXXX,              BL_TOGG, BL_DEC,  BL_INC,   BL_STEP, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX),
+    RALT(KC_N),           XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       RALT(KC_I), XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, LGUI(KC_MINS), XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, RALT(KC_E),    XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, RALT(KC_C), XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX),
+
+  LAYOUT_all(
+    RESET,                XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, TG(1),
+    XXXXXXX,              BL_TOGG, BL_DEC,  BL_INC,     BL_STEP, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
+    XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX),
 };
+
+void eeconfig_init_user(void) {
+  set_unicode_input_mode(UC_LNX);
+}
