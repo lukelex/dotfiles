7e2d5d5c8b4ef8f88458a151b8509f477e9bcce8
Lukas Alexandre <the.lukelex@gmail.com>
2021-06-10 15:51:31 +0200

    [Keyboards] Updating Exent & Praxis keymaps with

--- keyboards/praxis/keymap.c
+++ keyboards/praxis/keymap.c
@@ -21,31 +21,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PGUP,  KC_TAB,         KC_Q,       KC_W,    KC_E,          KC_R,    KC_T,    KC_Y,       KC_U,    KC_I,    KC_O,       KC_P,    KC_LBRC,       KC_RBRC, KC_BSPC,
     KC_PGDN,  LCTL_T(KC_ESC), KC_A,       KC_S,    KC_D,          KC_F,    KC_G,    KC_H,       KC_J,    KC_K,    KC_L,       KC_SCLN, KC_QUOT,       KC_ENT,
               KC_LSFT,        KC_Z,       KC_X,    KC_C,          KC_V,    KC_B,    KC_B,       KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH,       KC_RSFT, MO(4),
-              KC_LCTL,                    KC_LALT,                KC_SPC,  KC_LGUI,             LT(2, KC_ENT),                MO(3),                  MO(4)
+              KC_LALT,                    KC_LALT,                KC_SPC,  KC_LGUI,             LT(2, KC_ENT),                MO(3),                  MO(4)
   ),
 
   [1] = LAYOUT_all(
-    KC_ESC,   KC_TILD,        KC_1,       KC_2,    KC_3,          KC_4,    KC_5,    KC_6,       KC_7,    KC_8,    KC_9,       KC_0,    KC_MINS,       KC_EQL,  KC_BSLS,  KC_ESC,
-    KC_PGUP,  KC_TAB,         KC_Q,       KC_W,    KC_E,          KC_R,    KC_T,    KC_Y,       KC_U,    KC_I,    KC_O,       KC_P,    KC_LBRC,       KC_RBRC, KC_BSPC,
-    KC_PGDN,  KC_LCTL,        KC_A,       KC_S,    KC_D,          KC_F,    KC_G,    KC_H,       KC_J,    KC_K,    KC_L,       KC_SCLN, KC_QUOT,       KC_ENT,
-              KC_LSFT,        KC_Z,       KC_X,    KC_C,          KC_V,    KC_B,    KC_B,       KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH,       KC_RSFT, MO(4),
-              KC_LCTL,                    KC_LALT,                KC_SPC,  KC_LGUI,             LT(2, KC_ENT),    MO(3),                              MO(4)
+    _______,  _______,        _______,    _______, _______,       _______, _______, _______,    _______, _______, _______,    _______, _______,       _______, _______,  _______,
+    _______,  _______,        _______,    _______, _______,       _______, _______, _______,    _______, _______, _______,    _______, _______,       _______, _______,
+    _______,  KC_LCTL,        _______,    _______, _______,       _______, _______, _______,    _______, _______, _______,    _______, _______,       _______,
+              _______,        _______,    _______, _______,       _______, _______, _______,    _______, _______, _______,    _______, _______,       _______, _______,
+              _______,                    _______,                _______, _______,             _______,                      _______,                _______
   ),
 
   [2] = LAYOUT_all(
-    KC_ESC,   XXXXXXX,        KC_F1,      KC_F2,   KC_F3,         KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,      KC_F10,  KC_F11,        KC_F12,  XXXXXXX,  XXXXXXX,
-    KC_PGUP,  KC_TAB,         XXXXXXX,    KC_UP,   XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,    KC_PSCR, XXXXXXX,       XXXXXXX, KC_DEL,
-    KC_PGDN,  KC_LCTL,        KC_LEFT,    KC_DOWN, KC_RIGHT,      XXXXXXX, XXXXXXX, KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT,    XXXXXXX, XXXXXXX,       XXXXXXX,
-              KC_LSFT,        XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_MPRV,    KC_MNXT, KC_MPLY,       XXXXXXX, XXXXXXX,
-              KC_LCTL,                    KC_LALT,                KC_SPC,  KC_LGUI,             _______,          XXXXXXX,                            XXXXXXX
+    _______,  _______,        KC_F1,      KC_F2,   KC_F3,         KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,      KC_F10,  KC_F11,        KC_F12,  _______,  _______,
+    _______,  _______,        _______,    KC_UP,   _______,       _______, _______, _______,    _______, _______, _______,    KC_PSCR, _______,       _______, KC_DEL,
+    _______,  _______,        KC_LEFT,    KC_DOWN, KC_RIGHT,      _______, _______, KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT,    _______, _______,       _______,
+              _______,        _______,    _______, _______,       _______, _______, _______,    _______, _______, KC_MPRV,    KC_MNXT, KC_MPLY,       _______, _______,
+              _______,                    _______,                _______, _______,             _______,          _______,                            _______
   ),
 
   [3] = LAYOUT_all(
-    KC_ESC,   RALT(KC_N),     XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, RALT(KC_I), XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, LGUI(KC_MINS), XXXXXXX, XXXXXXX,  XXXXXXX,
-    KC_PGUP,  KC_TAB,         XXXXXXX,    XXXXXXX, RALT(KC_QUOT), XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, RALT(KC_O), XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
-    KC_PGDN,  KC_LCTL,        RALT(KC_A), XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, RALT(KC_E),    XXXXXXX,
-              KC_LSFT,        XXXXXXX,    XXXXXXX, RALT(KC_C),    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
-              KC_LCTL,                    KC_LALT,                KC_SPC,  KC_LGUI,             XXXXXXX,          XXXXXXX,                            XXXXXXX
+    _______,   RALT(KC_N),    _______,    _______, _______,       _______, _______, RALT(KC_I), _______, _______, _______,    _______, LGUI(KC_MINS), _______, _______,  _______,
+    _______,  _______,        _______,    _______, RALT(KC_QUOT), _______, _______, _______,    _______, _______, RALT(KC_O), _______, _______,       _______, _______,
+    _______,  _______,        RALT(KC_A), _______, _______,       _______, _______, _______,    _______, _______, _______,    _______, RALT(KC_E),    _______,
+              _______,        _______,    _______, RALT(KC_C),    _______, _______, _______,    _______, _______, _______,    _______, _______,       _______, _______,
+              _______,                    _______,                _______, _______,             _______,          _______,                            _______
   ),
 
   [4] = LAYOUT_all(
