c73fc60334433d7f7102acd787f719a13acbf8b0
Lukas Alexandre <the.lukelex@gmail.com>
2021-12-22 11:08:02 +0100

    [linux] Adding FA icons as PNG as Wired can't take SVGs

--- linux/scripts/usb-mounter.sh
+++ linux/scripts/usb-mounter.sh
@@ -16,12 +16,12 @@ result=$(udisksctl $action --block-device $drive)
 
 if [[ "$action" = "mount" ]]; then
   notify-send -a USB \
-              -i /usr/share/icons/FontAwesome-6/light/512x512/all/usb-drive.png \
+              -i ~/dotfiles/fonts/fa6/pngs/light/usb-drive.png \
               -h string:wired-tag:usb \
               'Drive' 'Successfully mounted'
 else
   notify-send -a USB \
-              -i /usr/share/icons/FontAwesome-6/light/512x512/all/usb-drive.png \
+              -i ~/dotfiles/fonts/fa6/pngs/light/usb-drive.png \
               -h string:wired-tag:usb \
               'Drive' 'Successfully unmounted'
 fi
