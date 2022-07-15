fdb21753d06d312928cf93c7dcd773f5d95fabab
Lukas Alexandre <the.lukelex@gmail.com>
2022-07-16 01:05:19 +0200

    [linux] Setup SSH Socket only once

--- linux/env.sh
+++ linux/env.sh
@@ -2,11 +2,11 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
-
 # Remember to enable any locales that you might need
 # sudo vim /etc/locale.gen && locale-gen
 
 # Custom executables
 export PATH="$HOME/dotfiles/linux/bin:$PATH"
 
-. ~/dotfiles/linux/setup/aliases
+source ~/dotfiles/linux/profile
+source ~/dotfiles/linux/setup/aliases
