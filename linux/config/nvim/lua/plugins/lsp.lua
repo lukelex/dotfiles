4a44a161115714766ba1bd95017e0eb4df4973d6
Lukas Alexandre <the.lukelex@gmail.com>
2023-03-25 23:01:50 +0100

    [vim] Adding LSP navigation key bindings

--- linux/config/nvim/lua/plugins/lsp.lua
+++ linux/config/nvim/lua/plugins/lsp.lua
@@ -15,8 +15,31 @@ require("mason-lspconfig").setup({
   }
 })
 
-require("lspconfig").ruby_ls.setup {}
-require("lspconfig").gdscript.setup {}
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").dockerls.setup {}
-require("lspconfig").docker_compose_language_service.setup {}
+local on_attach = function()
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+end
+
+local config = require("lspconfig")
+config.ruby_ls.setup {
+  on_attach = on_attach,
+}
+config.gdscript.setup {
+  on_attach = on_attach,
+}
+config.lua_ls.setup {
+  on_attach = on_attach,
+}
+config.dockerls.setup {
+  on_attach = on_attach,
+}
+config.vuels.setup {
+  on_attach = on_attach,
+}
+config.marksman.setup {
+  on_attach = on_attach,
+}
+config.docker_compose_language_service.setup {
+  on_attach = on_attach,
+}
