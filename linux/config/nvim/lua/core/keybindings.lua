c48887be14b4254ca7118108dfd80137e1c69721
Lukas Alexandre <the.lukelex@gmail.com>
2023-03-25 23:57:33 +0100

    [vim] Leveraging VimFugitive for a more comprehensive git blame

--- linux/config/nvim/lua/core/keybindings.lua
+++ linux/config/nvim/lua/core/keybindings.lua
@@ -5,4 +5,4 @@ vim.api.nvim_set_keymap("n", "<leader>h", ":nohlsearch<CR>", {})
 vim.api.nvim_set_keymap("n", "n", "nzz", {})
 vim.api.nvim_set_keymap("n", "N", "Nzz", {})
 
--- vim.api.nvim_set_keymap("v", "<Leader>b", ":<C-U>!git blame <C-R>=expand("%:p") <CR> \| sed -n <C-R>=line("'<") <CR>,<C-R>=line("'>") <CR>p <CR>")
+vim.api.nvim_set_keymap("n", "<leader>b", ":Git blame<CR>", {})
