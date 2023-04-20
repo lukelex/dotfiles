fa2a2405e8a8c4b5fab794eb2c7cabd539e98d74
Lukas Alexandre <the.lukelex@gmail.com>
2023-04-20 22:00:32 +0200

    [vim] Fixing LSP's autocompletion on Ruby and removing setup duplication

--- linux/config/nvim/lua/plugins/lsp/init.lua
+++ linux/config/nvim/lua/plugins/lsp/init.lua
@@ -1,19 +1,21 @@
+local servers = {
+  "bashls",
+  "solargraph",
+  "tsserver",
+  "vuels",
+  "html",
+  "cssls",
+  "marksman",
+  "lua_ls",
+  "sqlls",
+  "dockerls",
+  "docker_compose_language_service",
+  "vimls",
+}
+
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "bashls",
-    "ruby_ls",
-    "tsserver",
-    "vuels",
-    "html",
-    "cssls",
-    "marksman",
-    "lua_ls",
-    "sqlls",
-    "dockerls",
-    "docker_compose_language_service",
-    "vimls",
-  }
+  ensure_installed = servers
 })
 
 local on_attach = function()
@@ -27,41 +29,12 @@ end
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local config = require("lspconfig")
-config.ruby_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.gdscript.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.lua_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.dockerls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.tsserver.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.vuels.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.cssls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.marksman.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-config.docker_compose_language_service.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-require("plugins.lsp.appearance")
+for _, lsp in ipairs(servers) do
+  config[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
