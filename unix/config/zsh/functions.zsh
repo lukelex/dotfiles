e57e10dff9abb40bd527e3c8fc8154e65168a1a5
lukelex <the.lukelex@gmail.com>
2023-05-30 09:45:43 +0200

    [zsh] ZSH improvements
    * Reverse menu navigation with Shift+Tab
    * More flexible menu completion
    * Listing directories first
    * Don't store duplicate entries in the history
    * Fix bug on Reverse Search when mistreating columns
    * Split work related ENV loading into a separate file

--- unix/config/zsh/functions.zsh
+++ unix/config/zsh/functions.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 function fancy-history() {
-  local selection=$(history -t'%F %T' 0 | tac | fzf | cut -f 8- -d ' ')
+  local selection=$(history -t'%F %T' 0 | tac | fzf | tr -s '[:blank:]' ' ' | cut -f 5- -d ' ')
 
   zle kill-whole-line
   zle -U "$selection"
