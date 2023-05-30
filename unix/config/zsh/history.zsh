e552ebea8b3a6f8322f3d63c9061dc9ed949ca7c
Lukas Alexandre <the.lukelex@gmail.com>
2023-05-30 22:56:14 +0200

    [zsh] Extracting history settings into its own file

--- unix/config/zsh/history.zsh
+++ unix/config/zsh/history.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/zsh
 
+# history settings
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt EXTENDED_HISTORY
+setopt HIST_SAVE_NO_DUPS
+
 function fancy-history() {
   local selection=$(history -t'%F %T' 0 | tac | fzf | tr -s '[:blank:]' ' ' | cut -f 5- -d ' ')
 
