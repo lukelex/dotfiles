596cbb30b197cabe40caa84450a8683cde506edf
Lukas Alexandre <the.lukelex@gmail.com>
2023-09-14 12:37:51 +0200

    [vim] Refactoring basic keymaps
    Also:
    
    * Uses `U` instead `F5` for the UndoTree
    * Bring back git blame through `gb`

--- linux/config/nvim/lua/core/keymaps.lua
+++ linux/config/nvim/lua/core/keymaps.lua
@@ -1,19 +1,28 @@
+-- space as the leader key
 vim.g.mapleader = " "
 
+local function map(keycode, instruction)
+  vim.keymap.set("n", keycode, instruction, {
+    noremap = true,
+    silent = true
+  })
+end
+
 -- clean the current search
-vim.api.nvim_set_keymap("n", "<leader>h", ":nohlsearch<CR>", {})
+map("<leader>h", ":nohlsearch<CR>")
 
 -- always horizontally center search results
-vim.api.nvim_set_keymap("n", "n", "nzzzv", {})
-vim.api.nvim_set_keymap("n", "N", "Nzzzv", {})
+map("n", "nzzzv")
+map("N", "Nzzzv")
 
 -- navigate through panes with ctrl + hjkl
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+map("<C-J>", "<C-W><C-J>")
+map("<C-K>", "<C-W><C-K>")
+map("<C-L>", "<C-W><C-L>")
+map("<C-H>", "<C-W><C-H>")
 
 -- master fucking undo
-vim.keymap.set("n", "<F5>", ":UndotreeToggle<CR>")
+map("U", ":UndotreeToggle<CR>")
 
--- vim.api.nvim_set_keymap("n", "<leader>bl", ":Git blame<CR>", {})
+-- finger pointing time
+map("gb", ":Git blame<CR>")
