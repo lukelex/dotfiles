3430a4f3b1703ab80badc82a5afef8f1ae822192
Lukas Alexandre <the.lukelex@gmail.com>
2023-10-01 13:29:04 +0200

    [vim] Updating to IndentBlankLine v3

--- linux/config/nvim/lua/core/appearance.lua
+++ linux/config/nvim/lua/core/appearance.lua
@@ -7,9 +7,8 @@ require("colorizer").setup()
 
 require("gitsigns").setup()
 
-require("indent_blankline").setup {
-  show_current_context = true,
-  show_current_context_start = false,
+require("ibl").setup {
+  scope = { enabled = false },
 }
 
 require("lualine").setup({
