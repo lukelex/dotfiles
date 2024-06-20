29139be0b4ce09322da1df73501e350359878080
Lukas Alexandre <the.lukelex@gmail.com>
2024-06-20 11:03:18 +0200

    [vim] Linting some files

--- linux/config/nvim/lua/plugins/packer.lua
+++ linux/config/nvim/lua/plugins/packer.lua
@@ -1,8 +1,8 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -31,7 +31,7 @@ return packer.startup(function(use)
     after = "nvim-treesitter",
     requires = "nvim-treesitter/nvim-treesitter",
   })
-  use('nvim-treesitter/playground')
+  use("nvim-treesitter/playground")
   use("DataWraith/auto_mkdir")
   use("danro/rename.vim")
   use("schickling/vim-bufonly")
