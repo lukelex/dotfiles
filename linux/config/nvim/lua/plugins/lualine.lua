fc66bee38d00d61500bb9fc5cf6de04ddf6ad42b
Lukas Alexandre <the.lukelex@gmail.com>
2024-06-30 20:30:31 +0200

    [vim] Using Kaomojis as mode signs

--- linux/config/nvim/lua/plugins/lualine.lua
+++ linux/config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,19 @@
+local mode_map = {
+  n = "(ᴗ_ ᴗ。)",
+  nt = "(ᴗ_ ᴗ。)",
+  i = "(•̀ - •́ )",
+  R = "( •̯́ ₃ •̯̀)",
+  v = "(⊙ _ ⊙ )",
+  V = "(⊙ _ ⊙ )",
+  no ="(ﾉ°□°ﾉ )",
+  ["\22"] = "(⊙ _ ⊙ )",
+  t = "(⌐■_■)",
+  ['!'] = "(ﾉ°□°ﾉ )",
+  c = "(ﾉ°□°ﾉ )",
+  s = "SUB"
+}
+ -- (╯°□°）╯︵ ( ͜。͡ʖ͜。)
+
 require("lualine").setup({
   options = {
     theme = "railscasts",
@@ -13,7 +29,19 @@ require("lualine").setup({
     lualine_z = { "diagnostics", "filetype" },
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = {
+      {
+        "mode",
+        icons_enabled = true,
+        separator = {
+          left = "",
+          right = ""
+        },
+        fmt = function()
+          return mode_map[vim.api.nvim_get_mode().mode] or vim.api.nvim_get_mode().mode
+        end
+      },
+    },
     lualine_b = { "branch", "diff" },
     lualine_c = { },
     lualine_x = { "encoding", "fileformat" },
@@ -21,4 +49,3 @@ require("lualine").setup({
     lualine_z = { "location" }
   },
 })
-
