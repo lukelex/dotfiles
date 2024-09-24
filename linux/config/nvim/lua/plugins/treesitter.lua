516fdd50824e7bb07a8bf0f2885a3d615ff983b1
Lukas Alexandre <the.lukelex@gmail.com>
2024-09-24 15:08:38 +0200

    [vim] Pre-install a couple of TS parsers & add `af` & `if` function text objects

--- linux/config/nvim/lua/plugins/treesitter.lua
+++ linux/config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,14 @@
 require("nvim-treesitter.configs").setup {
+  ensure_installed = {
+    "ruby",
+    "javascript",
+    "typescript",
+    "yaml",
+    "markdown",
+    "css",
+    "html",
+    "bash",
+  },
   sync_install = false,
   auto_install = true,
   highlight = {
@@ -16,6 +26,9 @@ require("nvim-treesitter.configs").setup {
 
         ["am"] = { query = "@function.outer", desc = "Select around a method/function definition" },
         ["im"] = { query = "@function.inner", desc = "Select inside a method/function definition" },
+
+        ["af"] = { query = "@function.outer", desc = "Select around a method/function definition" },
+        ["if"] = { query = "@function.inner", desc = "Select inside a method/function definition" },
       }
     }
   }
