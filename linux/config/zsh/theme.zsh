f15334ed9be82909baac15a1a05896543c35c4a0
Lukas Alexandre <the.lukelex@gmail.com>
2025-06-11 13:19:40 +0200

    [zsh] Fixing branch icon

--- linux/config/zsh/theme.zsh
+++ linux/config/zsh/theme.zsh
@@ -14,7 +14,7 @@ precmd() { vcs_info }
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr '%F{#DA4939}*%f'
 zstyle ':vcs_info:*' stagedstr '%F{#A5C261}^%f'
-zstyle ':vcs_info:git:*' formats '%F{#6D9CBE}[ %24>..>%b%<<%u%c%F{#6D9CBE}]%f'
+zstyle ':vcs_info:git:*' formats '%F{#6D9CBE}[ %24>..>%b%<<%u%c%F{#6D9CBE}]%f'
 
 PROMPT="%F{#6D9CBE}$ICON%f %20>...>%B%F{#AF5F00}%1~%<<%f%b %F{#6D9CBE}$INPUT_ICON%f "
 RPROMPT='${vcs_info_msg_0_}%F{#519F50}%(1j.[j:%j].)%f'
