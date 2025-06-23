f3840b1f489806d97db8a195e977203d027739ea
Lukas Alexandre <the.lukelex@gmail.com>
2025-06-23 22:33:43 +0200

    [vim] Revamping Lualine config
    * Removed the updates on cursor move to avoid lag
    * Truncating long branch names
    * Better file diff output
    * Files paths now include their immediate directory name
    * Simpler and less annoying LSP status indicator
    * Easier to read icons
    * Removed Kaomoji mode indicators

--- linux/config/nvim/lua/plugins/lualine.lua
+++ linux/config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,19 @@
+local function lsp_count()
+  local clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
+
+  return #clients
+end
+
+local function pathd(str)
+  local parts = vim.split(str, "/", { plain = true })
+
+  if #parts >= 2 then
+    return parts[#parts - 1] .. "/" .. parts[#parts]
+  else
+    return str
+  end
+end
+
 return {
   {
     "nvim-lualine/lualine.nvim",
@@ -5,35 +21,57 @@ return {
       "nvim-tree/nvim-web-devicons"
     },
     config = function()
-      local mode_map = {
-        n = "(ᴗ_ ᴗ。)",
-        nt = "(ᴗ_ ᴗ。)",
-        i = "(•̀ - •́ )",
-        R = "( •̯́ ₃ •̯̀)",
-        v = "(⊙ _ ⊙ )",
-        V = "(⊙ _ ⊙ )",
-        no = "(ﾉ°□°ﾉ )",
-        ["\22"] = "(⊙ _ ⊙ )",
-        t = "(⌐■_■)",
-        ['!'] = "(ﾉ°□°ﾉ )",
-        c = "(ﾉ°□°ﾉ )",
-        s = "SUB"
-      }
-      -- (╯°□°）╯︵ ( ͜。͡ʖ͜。)
-
       require("lualine").setup({
         options = {
           theme = "railscasts",
           icons_enabled = true,
           globalstatus = true,
+          refresh = {
+            winbar = 300,
+            tabline = 1000,
+            statusline = 1000,
+            events = {
+              "WinEnter",
+              "BufEnter",
+              "BufWritePost",
+              "SessionLoadPost",
+              "FileChangedShellPost",
+              "VimResized",
+              "Filetype",
+              -- "CursorMoved",
+              -- "CursorMovedI",
+              "ModeChanged",
+            },
+          }
         },
         winbar = {
-          lualine_a = { "filename" },
-          lualine_y = { "lsp_status" },
+          lualine_a = { { "filename", path = 1, fmt = pathd } },
+          lualine_b = {
+            {
+              "diff",
+              symbols = { added = " ", modified = " ", removed = " " },
+              diff_color = {
+                added = { fg = "#87AF5F" },
+                modified = { fg = "#6D9CBE" },
+                removed = { fg = "#DA4939" },
+              },
+            }
+          },
+          lualine_y = { { lsp_count, icon = " " } },
           lualine_z = { "diagnostics", "filetype" },
         },
         inactive_winbar = {
-          lualine_a = { "filename" },
+          lualine_a = { { "filename", path = 1, fmt = pathd } },
+          lualine_b = { {
+            "diff",
+            symbols = { added = " ", modified = " ", removed = " " },
+            diff_color = {
+              added = { fg = "#87AF5F" },
+              modified = { fg = "#6D9CBE" },
+              removed = { fg = "#DA4939" },
+            },
+          } },
+          lualine_y = { { lsp_count, icon = " " } },
           lualine_z = { "diagnostics", "filetype" },
         },
         sections = {
@@ -45,16 +83,25 @@ return {
                 left = "",
                 right = ""
               },
-              fmt = function()
-                return mode_map[vim.api.nvim_get_mode().mode] or vim.api.nvim_get_mode().mode
-              end
             },
           },
-          lualine_b = { "branch", "diff" },
+          lualine_b = {
+            {
+              "branch",
+              icon = "",
+              fmt = function(str)
+                if vim.api.nvim_strwidth(str) > 25 then
+                  return ("%s…"):format(str:sub(1, 24))
+                end
+
+                return str
+              end,
+            }
+          },
           lualine_c = {},
           lualine_x = { "encoding", "fileformat" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" }
+          lualine_y = { { "progress", icon = " " } },
+          lualine_z = { { "location", icon = "" } }
         },
       })
     end
