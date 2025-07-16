baccaa77d14b795e1650b7e43b17f71be0201549
Lukas Alexandre <the.lukelex@gmail.com>
2025-07-16 12:49:47 +0200

    [vim] Adding context aware Svelte commenting

--- linux/config/nvim/lua/plugins/others.lua
+++ linux/config/nvim/lua/plugins/others.lua
@@ -3,20 +3,21 @@ return {
     -- Editor improvements
     "DataWraith/auto_mkdir",
     "schickling/vim-bufonly",
-    { "danro/rename.vim",                 event = { "BufRead" } },
-    { "tpope/vim-commentary",             event = { "BufRead" } },
-    { "tpope/vim-repeat",                 event = { "BufRead" } },
-    { "tpope/vim-surround",               event = { "BufRead" } },
-    { "mfussenegger/nvim-lint",           event = { "BufRead" } },
+    { "danro/rename.vim",                            event = { "BufRead" } },
+    { "tpope/vim-commentary",                        event = { "BufRead" } },
+    { "tpope/vim-repeat",                            event = { "BufRead" } },
+    { "tpope/vim-surround",                          event = { "BufRead" } },
+    { "mfussenegger/nvim-lint",                      event = { "BufRead" } },
 
     -- Language improvements
-    { "ron-rs/ron.vim",                   event = { "BufRead" } },
-    { "baskerville/vim-sxhkdrc",          event = { "BufRead" } },
-    { "kmonad/kmonad-vim",                event = { "BufRead" } },
-    { "slint-ui/vim-slint",               event = { "BufRead" } },
-    { "luizribeiro/vim-cooklang",         event = { "BufRead" } },
-    { "mustache/vim-mustache-handlebars", event = { "BufRead" } },
-    { "isobit/vim-caddyfile",             event = { "BufRead" } },
-    { "terrastruct/d2-vim",               event = { "BufRead" } },
+    { "ron-rs/ron.vim",                              event = { "BufRead" } },
+    { "baskerville/vim-sxhkdrc",                     event = { "BufRead" } },
+    { "kmonad/kmonad-vim",                           event = { "BufRead" } },
+    { "slint-ui/vim-slint",                          event = { "BufRead" } },
+    { "luizribeiro/vim-cooklang",                    event = { "BufRead" } },
+    { "mustache/vim-mustache-handlebars",            event = { "BufRead" } },
+    { "isobit/vim-caddyfile",                        event = { "BufRead" } },
+    { "terrastruct/d2-vim",                          event = { "BufRead" } },
+    { "JoosepAlviste/nvim-ts-context-commentstring", event = { "BufRead" } }
   }
 }
