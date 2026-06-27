a8039e6496bca83a8bbab1a1daccb64ed42315e7
Lukas Alexandre <the.lukelex@gmail.com>
2026-06-27 16:23:29 +0200

    [linux] General improvements

--- linux/config/nvim/lua/plugins/noice.lua
+++ linux/config/nvim/lua/plugins/noice.lua
@@ -2,6 +2,61 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {},
+  config = function()
+    require('noice').setup {
+      -- lsp = {
+      --   enable = false
+      -- },
+      presets = {
+        long_message_to_split = true,
+        lsp_doc_border = true,
+      },
+      routes = {
+        { filter = { find = "lines" }, skip = true }, -- skip yank and paste
+        {
+          filter = {
+            event = "lsp",
+            kind = "progress",
+            cond = function(message)
+              local client = vim.tbl_get(message.opts, "progress", "client")
+              return client == "solargraph"
+            end,
+          },
+          opts = { skip = true },
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 5,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    }
+  end,
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
