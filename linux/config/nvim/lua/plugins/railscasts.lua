bed5c63767bcfbaa5459cc50f7db71979e13230e
Lukas Alexandre <the.lukelex@gmail.com>
2026-09-25 00:04:40 +0200

    [nvim] configure Railscasts through setup API

--- linux/config/nvim/lua/plugins/railscasts.lua
+++ linux/config/nvim/lua/plugins/railscasts.lua
@@ -2,7 +2,8 @@ return {
   "lukelex/railscasts.nvim",
   version = ">=2",
   priority = 1000,
-  config = function()
+  opts = {},
+  init = function()
     vim.cmd.colorscheme "railscasts"
   end
 }
